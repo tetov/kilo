a64074c3d7ea484579d10e5b1ad5732537693aa8
tetov <sonya.mamurin@gmail.com>
2018-01-21 16:44:40 +0100

    Commiting old edits

--- kilo.c
+++ kilo.c
@@ -22,6 +22,7 @@ typedef struct erow {
 } erow;
 
 struct editorConfig {
+    int cx, cy;
     int screenrows;
     int screencols;
     struct termios orig_termios;
@@ -157,6 +158,10 @@ void editorRefreshScreen() {
 
     editorDrawRows(&ab);
 
+    char buf[32];
+    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+    abAppend(&ab, buf, strlen(buf));
+
     abAppend(&ab, "\x1b[h", 3);
     abAppend(&ab, "\x1b[?25h", 6);
 
@@ -181,6 +186,9 @@ void editorProcessKeypress() {
 /* init */
 
 void initEditor() {
+    E.cx = 0;
+    E.cy = 0;
+
     if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
 }
 
